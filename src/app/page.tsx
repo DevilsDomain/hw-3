a669fe1d636ef696e840e5d4e71152ec2092ecd3
DevilsDomain <alaraerzincan@gmail.com>
2023-03-17 15:54:39 +0100

    api works on the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+'use client';
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import useSWR from 'swr';
 
-const inter = Inter({ subsets: ['latin'] })
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
+
+
 export default function Home() {
+  const { data, error } = useSWR('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0', fetcher)
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
   return (
     <main className={styles.main}>
-      <p>Hello</p>
+      <p>hello</p>
+      {data.results.map((pokemon, pokemonIndex) => {
+        return(
+          <p key={pokemonIndex}>{pokemon.name}</p>
+        );
+      })}
     </main>
   )
 }
